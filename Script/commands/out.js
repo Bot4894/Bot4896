5cd02d579246109c351cad66c882e4adbc1347f4
Bot4894 <mdfahimmdfahim1080@gmail.com>
2025-06-30 00:30:24 +0600

    Update out.js

--- Script/commands/out.js
+++ Script/commands/out.js
@@ -1,7 +1,7 @@
 module.exports.config = {
   name: "leave",
   version: "1.0.0",
-  hasPermssion: 2,
+  hasPermssion: 0,
   credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
   description: "out box",
   commandCategory: "Admin",
